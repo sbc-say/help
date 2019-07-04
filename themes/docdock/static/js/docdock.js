06897671188bf881a773a5932c98f2ba6ddc6488
Yuya Ushijima <yuya.ushijima@g.softbank.co.jp>
2019-07-04 14:04:43 +0900

    Mermaid.jsの有効化

--- themes/docdock/static/js/docdock.js
+++ themes/docdock/static/js/docdock.js
@@ -77,12 +77,11 @@ jQuery(document).ready(function () {
         $(e.trigger).attr('aria-label', 'Link copied to clipboard!').addClass('tooltipped tooltipped-s');
     });
 
-});
-
-
-$('code.language-mermaid').each(function(index, element) {
-  var content = $(element).html().replace(/&amp;/g, '&');
-  $(element).parent().replaceWith('<div class="mermaid" align="center">' + content + '</div>');
+    // Enable mermaid code block // seems not working
+    $('code.language-mermaid').each(function(index, element) {
+        var content = $(element).html().replace(/&amp;/g, '&');
+        $(element).parent().replaceWith('<div class="mermaid" align="center">' + content + '</div>');
+    });
 });
 
 
