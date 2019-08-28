ea015f335f648219e87034dbc3e6edbda40b9169
Yuya Ushijima <yuya.ushijima@g.softbank.co.jp>
2019-08-28 21:22:48 +0900

    OSSエクスポートの実装

--- .circleci/oss-deploy.sh
+++ .circleci/oss-deploy.sh
@@ -3,20 +3,26 @@
 # エラー時、実行を止める
 set -e
 
-DEPLOY_DIR=oss-deploy
+#DEPLOY_DIR=oss-deploy
 
 # gitの諸々の設定
-git config --global push.default simple
-git config --global user.email $(git --no-pager show -s --format='%ae' HEAD)
-git config --global user.name $CIRCLE_USERNAME
+#git config --global push.default simple
+#git config --global user.email $(git --no-pager show -s --format='%ae' HEAD)
+#git config --global user.name $CIRCLE_USERNAME
 
 # oss-deployブランチをossdeployディレクトリにクローン
-git clone -q --branch=oss-deploy $CIRCLE_REPOSITORY_URL $DEPLOY_DIR
+#git clone -q --branch=oss-deploy $CIRCLE_REPOSITORY_URL $DEPLOY_DIR
 
-# rsyncでhugoで生成したHTMLをOSSへコピー
-cd $DEPLOY_DIR
+# 作業Directoryへ移動
+#cd $DEPLOY_DIR
+
+## Replace HTML output for OSS deployment
+$ find ./docs/* -type f -name index.html -exec sed -i -e  's/\/">/\/index.html">/g' {} \;
+$ find ./docs/* -type f -name index.html -exec sed -i -e 's/help\/image/image/g' {} \;
+
+## Deploy to OSS bucket
 ossutil ls oss://technical-reference
-ossutil cp -f  ./ oss://technical-reference/  --recursive
+ossutil cp -f  ./docs oss://technical-reference/  --recursive
 ossutil ls oss://technical-reference
 
 # EOF
